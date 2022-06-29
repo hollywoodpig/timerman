77de6d2773de344bb152145608894311e891098d
hollywoodpig <nekitkld@gmail.com>
2022-06-29 22:29:01 +0200

    added pwa

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,15 @@
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement
 );
 root.render(<App />);
+
+serviceWorkerRegistration.register();
+
+window.addEventListener('beforeinstallprompt', function (e) {
+	console.log(123);
+});
